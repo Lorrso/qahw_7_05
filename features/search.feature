70d5dc8832beee91b34060eedd4ce9dd3b24a62e
lorrso <anreich0@gmail.com>
2024-05-26 20:12:53 +0300

    второе задание доработано

--- features/search.feature
+++ features/search.feature
@@ -1,24 +1,26 @@
-Feature: Buying a standart ticket
-    Scenario Outline: Should purchase a standart ticket
+Feature: Cinema testing
+    Scenario: Should purchase a standart ticket
         Given user is on "/client/index.php" page
-        When user goes to the page of the "a[data-seance-id='199']" hall
+        When user select next date
+        When user goes to the page of the Micky Mouse hall
         When selects a "standart" ticket
+        When presses the ".acceptin-button" to buying a ticket
         When presses the ".acceptin-button" to confirm the reservation
-        Then user sees the ".ticket__info-qr"
+        Then user sees "Электронный билет" title
 
-Feature: Buying a VIP ticket
-    Scenario Outline: Should purchase a VIP ticket
+    Scenario: Should purchase a VIP ticket
         Given user is on "/client/index.php" page
-        When user goes to the page of the "a[data-seance-id='199']" hall
+        When user select next date
+        When user goes to the page of the Micky Mouse hall
         When selects a "vip" ticket
         When presses the ".acceptin-button" to buying a ticket
         When presses the ".acceptin-button" to confirm the reservation
-        Then user sees the ".ticket__info-qr"
+        Then user sees "Электронный билет" title
 
-Feature: Trying to buy an already purchased ticket
-    Scenario Outline: Should not lead to the booking page
+    Scenario: Should not lead to the booking page when trying to purchase a disabled ticket
         Given user is on "/client/index.php" page
-        When user goes to the page of the "a[data-seance-id='199']" hall
+        When user select next date
+        When user goes to the page of the Micky Mouse hall
         When selects a "disabled" ticket
         When presses the ".acceptin-button" to buying a ticket
         Then user stays on the "/client/hall.php" page
--- features/step_definitions/search.steps.js
+++ features/step_definitions/search.steps.js
@@ -2,7 +2,7 @@ const puppeteer = require("puppeteer");
 const chai = require("chai");
 const expect = chai.expect;
 const { Given, When, Then, Before, After, setDefaultTimeout } = require("cucumber");
-const { clickElement, chooseSeat } = require("../../lib/commands.js");
+const { clickElement, chooseSeat, getText } = require("../../lib/commands.js");
 
 setDefaultTimeout(120000);
 Before(async function () {
@@ -20,15 +20,17 @@ After(async function () {
 
 Given("user is on {string} page", async function (string) {
   return await this.page.goto(`http://qamid.tmweb.ru${string}`, {
-    setTimeout: 45000,
+    setTimeout: 60000,
   });
 });
 
-When("user goes to the page of the {string} hall", async function (string) {
-  await clickElement(this.page, "a:nth-child(2)");
-  await page.mouse.wheel({deltaY: 700});
-  await clickElement(this.page, "a[data-seance-id='199']");
-  return await clickElement(this.page, string);
+When("user select next date", async function () {
+  return await clickElement(this.page, "a:nth-child(2)");
+});
+
+When("user goes to the page of the Micky Mouse hall", async function () {
+  await this.page.mouse.wheel({deltaY: 700});
+  return await clickElement(this.page, "a[data-seance-id='199']");
 });
 
 When("selects a {string} ticket", async function (string) {
@@ -43,9 +45,9 @@ When("presses the {string} to confirm the reservation", async function (string) 
   return await clickElement(this.page, string);
 });
 
-Then("user sees the {string}", async function (string) {
-  const actual = await this.page.waitForSelector('.ticket__info-qr', { visible: true, });
-  const expected = await this.page.waitForSelector(string, { visible: true, });
+Then("user sees {string} title", async function (string) {
+  const actual = await getText(this.page, "h2[class='ticket__check-title']");
+  const expected = await string;
   expect(actual).contains(expected);
 });
 
